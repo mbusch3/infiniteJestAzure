8cfd002c32c31b8d222a7f938cca53c1153ff2cc
mbusch3 <morganjbusch@gmail.com>
2021-12-08 15:09:34 -0700

    Lots of npm updates and work on the API

--- api/GetUsers/index.js
+++ api/GetUsers/index.js
@@ -1,13 +1,123 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // VERSION 1 - What am I missing to make this readable?
+    // const sql = require('mssql');
 
+    // const AZURE_CONNECTION_STRING = process.env["DB_CONNECTION_STRING"];
+
+    // sql.connect(AZURE_CONNECTION_STRING, function(error) {
+    //     if (error) throw error;
+    //     console.log("Hello, there!");
+    //     var userQuery = new sql.Request();
+    //     userQuery.query("SELECT * FROM Users FOR JSON PATH", function (error, results) {
+    //         if (error) throw error;
+    //         console.log(results);
+    //         context.res = {
+    //             body: results
+    //         }
+    //     });
+    // });
+
+
+    // VERSION 2
+    // const { Connection, Request } = require("tedious");
+
+    // // Create connection to database
+    // const config = {
+    //   authentication: {
+    //     options: {
+    //       userName: "cloudadmin",
+    //       password: "**********"
+    //     },
+    //     type: "default"
+    //   },
+    //   server: "infinite-jest.database.windows.net",
+    //   options: {
+    //     database: "infiniteJest",
+    //     encrypt: true
+    //   }
+    // };
+    
+    // /* 
+    //     //Use Azure VM Managed Identity to connect to the SQL database
+    //     const config = {
+    //         server: process.env["db_server"],
+    //         authentication: {
+    //             type: 'azure-active-directory-msi-vm',
+    //         },
+    //         options: {
+    //             database: process.env["db_database"],
+    //             encrypt: true,
+    //             port: 1433
+    //         }
+    //     };
+    
+    //     //Use Azure App Service Managed Identity to connect to the SQL database
+    //     const config = {
+    //         server: process.env["db_server"],
+    //         authentication: {
+    //             type: 'azure-active-directory-msi-app-service',
+    //         },
+    //         options: {
+    //             database: process.env["db_database"],
+    //             encrypt: true,
+    //             port: 1433
+    //         }
+    //     });
+    
+    // */
+    
+    // const connection = new Connection(config);
+    
+    // // Attempt to connect and execute queries if connection goes through
+    // connection.on("connect", err => {
+    //   if (err) {
+    //     console.error(err.message);
+    //   } else {
+    //     queryDatabase();
+    //   }
+    // });
+    
+    // connection.connect();
+    
+    // function queryDatabase() {
+    //   console.log("Reading rows from the Table...");
+    
+    //   // Read all rows from table
+    //   const request = new Request(
+    //     `SELECT *
+    //      FROM [dbo].[Users]`,
+    //     (err, rowCount) => {
+    //       if (err) {
+    //         console.error(err.message);
+    //       } else {
+    //         console.log(`${rowCount} row(s) returned`);
+    //       }
+    //     }
+    //   );
+    //   var responseMessage = '';
+    //   request.on("row", columns => {
+    //     columns.forEach(column => {
+    //       responseMessage += ("%s\t%s", column.metadata.colName, column.value);
+    //     });
+    //   });
+
+    //   request.on("requestCompleted", function() {
+    //       context.res = {
+    //           status: 200,
+    //           body: "This here means that everything ran!\n" + responseMessage,
+    //       }
+    //   })
+    
+    //   connection.execSql(request);
+    // }
+
+
+
+    
+    // Quick connection test
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: "Hello from the API"
     };
+
 }
